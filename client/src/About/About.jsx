c0720d2c0f64717322a1f1432a59f1edd751e82a
Jorge Lopez <j.luis.jl7@gmail.com>
2023-08-13 00:15:43 -0600

    Feat: updating responsive design for home page

--- client/src/About/About.jsx
+++ client/src/About/About.jsx
@@ -5,14 +5,14 @@ export default function About() {
   return (
     <div>
       <section id="start" className="flex items-center justify-center">
-        <div className="flex w-8/12 mt-28">
-          <div className="text-left text-xl">
-            <h1 className="text-3xl font-semibold mb-5">
+        <div className="flex flex-col items-center md:flex-row w-10/12 lg:w-8/12 mt-8 lg:mt-28">
+          <div className="text-center order-last md:text-left md:order-first">
+            <h1 className="text-xl lg:text-3xl font-semibold mb-5">
               Hello! <br />
               My name is Jorge López and I'm a software engineer intern at
               Oracle.
             </h1>
-            <p>
+            <p className="text-base lg:text-xl">
               My first tech projects started back in 2013 with vanilla HTML and
               CSS websites, fast forward to today, I've learned great
               technologies that helped me to be a great developer and create
@@ -22,24 +22,26 @@ export default function About() {
               crypto network.
             </p>
             <p>These is the tech stack I've been working with:</p>
-            <div className="flex my-5 font-semibold">
-              <div className="mr-20 ml-20">
+            <div className="flex my-5 md:text-base font-semibold justify-center">
+              <div className="mr-5 ml-5 md:mr-20 md:ml-20">
                 <p>• Java</p>
                 <p>• Python</p>
                 <p>• Docker</p>
                 <p>• PostgreSQL</p>
                 <p>• JavaScript</p>
               </div>
-              <div className="ml-20">
+              <div className="ml-5 md:ml-20">
                 <p>• React</p>
                 <p>• Django</p>
                 <p>• MongoDB</p>
                 <p>• FastAPI</p>
               </div>
             </div>
-            <p className="text-lg">Also learning new things (DevOps & React)</p>
+            <p className="text-sm lg:text-xl">
+              Also learning new things (DevOps & React)
+            </p>
           </div>
-          <div className="w-4/5">
+          <div className="w-4/12 md:w-4/5">
             <img src={me} alt="" />
           </div>
         </div>
